c2f7068d0af600e8f6dbc8d6599b33e44ded459d
genebarringer <gene.barringer@gmail.com>
2020-10-20 14:32:56 -0500

    added the logs directory to gitignore

---
+++ src/test/java/org/gene/filedemo/service/FileStorageServiceTest.java
@@ -0,0 +1,30 @@
+package org.gene.filedemo.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileStorageServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void listFilesInStorage() {
+    }
+
+    @Test
+    void storeFile() {
+    }
+
+    @Test
+    void loadFileAsResource() {
+    }
+}
